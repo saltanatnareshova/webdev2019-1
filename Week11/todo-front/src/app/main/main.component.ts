ed9604a495d63526bd516bd202d06851399d3abc
Adilet Absatov <adl.absatov@gmail.com>
2019-04-13 00:42:34 +0600

    GetTaksLists works

--- Week11/todo-front/src/app/main/main.component.ts
+++ Week11/todo-front/src/app/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProviderService } from '../shared/services/provider.service';
+import { ITaskList } from '../shared/models/models';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  public taskLists: ITaskList[] = [];
+
+  constructor(private provider: ProviderService) { }
 
   ngOnInit() {
+    this.provider.getTaskLists().then(res => {
+      this.taskLists = res;
+    });
   }
 
 }
--- Week11/todo-front/src/app/shared/services/provider.service.ts
+++ Week11/todo-front/src/app/shared/services/provider.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {MainService} from './main.service';
 import {HttpClient} from '@angular/common/http';
+import { ITaskList } from '../models/models';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +10,9 @@ export class ProviderService extends MainService {
 
   constructor(http: HttpClient) {
     super(http);
+  }
+
+  getTaskLists(): Promise<ITaskList[]> {
+    return this.get('http://127.0.0.1:8000/api/task_lists/', {});
   }
 }
