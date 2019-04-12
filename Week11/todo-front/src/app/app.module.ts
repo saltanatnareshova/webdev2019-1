7f5bafb716b7afc2db86d07267f20839cccf7c8a
Adilet Absatov <adl.absatov@gmail.com>
2019-04-12 22:34:31 +0600

    All components, interfaces, services created

--- Week11/todo-front/src/app/app.module.ts
+++ Week11/todo-front/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProviderService } from './shared/services/provider.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ProviderService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
